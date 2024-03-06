8cdd683ad7f8274feaedc4ac4997c75fc505a4f3
Trysten M <nick.tristen22@gmail.com>
2024-03-06 05:40:49 +0200

    update(api/routes): add works routes

--- routes/_worksRouter.js
+++ routes/_worksRouter.js
@@ -1,5 +1,31 @@
 import {Router} from "express";
+import {AuthMiddleware} from "../middlewares/authMiddleware.js";
+import {routeNames} from "../helpers/routesHelper.js";
+import WorkController from "../controllers/workController.js";
 
 const router = new Router()
+    .get(
+        routeNames.WORKS.GET_ALL,
+        WorkController.getAll
+    )
+    .get(
+        routeNames.WORKS.GET_ONE,
+        WorkController.getOne
+    )
+    .post(
+        routeNames.WORKS.CREATE,
+        AuthMiddleware,
+        WorkController.create
+    )
+    .patch(
+        routeNames.WORKS.UPDATE,
+        AuthMiddleware,
+        WorkController.update
+    )
+    .delete(
+        routeNames.WORKS.DELETE,
+        AuthMiddleware,
+        WorkController.delete
+    )
 
 export default router
