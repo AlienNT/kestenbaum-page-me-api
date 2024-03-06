da2378e747d1e1947e66f155250789abb55387b7
Trysten M <nick.tristen22@gmail.com>
2024-03-06 05:41:54 +0200

    add(api/helpers): add routes names helper

---
+++ helpers/routesHelper.js
@@ -0,0 +1,23 @@
+export const routeNames = {
+    CONTACTS: {
+        GET_ONE: '/contacts/:id',
+        GET_ALL: '/contacts',
+        CREATE: '/contacts',
+        UPDATE: '/contacts/:id',
+        DELETE: '/contacts/:id',
+    },
+    SKILLS: {
+        GET_ONE: '/skills/:id',
+        GET_ALL: '/skills',
+        CREATE: '/skills',
+        UPDATE: '/skills/:id',
+        DELETE: '/skills/:id',
+    },
+    WORKS: {
+        GET_ONE: '/works/:id',
+        GET_ALL: '/works',
+        CREATE: '/works',
+        UPDATE: '/works/:id',
+        DELETE: '/works/:id',
+    }
+}
