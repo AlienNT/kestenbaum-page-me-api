306bc52bf55d3958a5a2bafa8c046267029787e7
Trysten M <nick.tristen22@gmail.com>
2024-03-06 06:01:28 +0200

    add(api/controllers): create skill controller

---
+++ controllers/skillController.js
@@ -0,0 +1,32 @@
+class SkillController {
+    async gelAll(req, res) {
+        try {
+        } catch (e) {
+        }
+    }
+
+    async gelOne(req, res) {
+        try {
+        } catch (e) {
+        }
+    }
+
+    async create(req, res) {
+        try {
+        } catch (e) {
+        }
+    }
+
+    async update(req, res) {
+        try {
+        } catch (e) {
+        }
+    }
+
+    async delete(req, res) {
+        try {
+        } catch (e) {
+        }
+    }
+}
+export default new SkillController()
