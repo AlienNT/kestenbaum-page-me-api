2e12d0c1fcbadd2d82dcb5b6c75c6862929ff76b
Trysten M <nick.tristen22@gmail.com>
2024-03-06 07:28:11 +0200

    add(api/services): document fields service

---
+++ services/DocumentFieldService.js
@@ -0,0 +1,28 @@
+class DocumentFieldService {
+    requestSkillFields(req) {
+        return this.#getFields(req?.body, 'title', 'img')
+    }
+
+    requestWorkFields(req) {
+        return this.#getFields(req?.body, 'title', 'img', 'link', 'category')
+    }
+
+    requestContactFields(req) {
+        return this.#getFields(req?.body, 'title', 'value')
+    }
+
+    #getFields(body, ...fields) {
+        if (!fields?.length) return null
+
+        const res = {}
+
+        fields.forEach(field => {
+            if (body.hasOwnProperty(field)) res[field] = body[field]
+        })
+
+        return !Object.keys(res)?.length ? null : res
+    }
+}
+
+
+export default new DocumentFieldService()
