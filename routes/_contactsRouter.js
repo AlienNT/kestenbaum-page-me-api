b22873781f383cc0a9274ee6087f10eb4b3a4b92
Trysten M <nick.tristen22@gmail.com>
2024-03-06 06:05:25 +0200

    update(api/routes): update contacts routes

--- routes/_contactsRouter.js
+++ routes/_contactsRouter.js
@@ -1,5 +1,31 @@
 import {Router} from "express";
+import {AuthMiddleware} from "../middlewares/authMiddleware.js";
+
+import {routeNames} from "../helpers/routesHelper.js";
+import ContactController from "../controllers/contactController.js";
 
 const router = new Router()
-
+    .get(
+        routeNames.CONTACTS.GET_ALL,
+        ContactController.gelAll
+    )
+    .get(
+        routeNames.CONTACTS.GET_ONE,
+        ContactController.gelOne
+    )
+    .post(
+        routeNames.CONTACTS.CREATE,
+        AuthMiddleware,
+        ContactController.create
+    )
+    .patch(
+        routeNames.CONTACTS.UPDATE,
+        AuthMiddleware,
+        ContactController.update
+    )
+    .delete(
+        routeNames.CONTACTS.DELETE,
+        AuthMiddleware,
+        ContactController.delete
+    )
 export default router
