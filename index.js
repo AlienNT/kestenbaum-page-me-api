32a3b0d32f6ecfd079f6ece2a44ce8deb1ee1e0a
Trysten M <nick.tristen22@gmail.com>
2024-03-06 05:20:58 +0200

    update(api): add db connection; add server start

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
 import Express from 'express'
+import mongoose from "mongoose";
+
+import routes from "./routes/index.js";
+import config from "./config.js";
+import bodyParser from "body-parser";
+
+import cors from "cors";
+import {errorResponse} from "./helpers/responseHelper.js";
 
 
-const App = new Express()
+let isConnect = false
+
+const API = new Express()
+    .use(checkConnection)
+    .use(bodyParser.json())
+    .use(cors())
+    .use(config.API_ROUTE, routes)
+
+async function server() {
+    try {
+        await mongoose.connect(config.DB)
+            .then(() => isConnect = true)
+            .catch(() => isConnect = false)
+
+        API.listen(config.PORT, () => {
+            console.log('server started in port: ', config.PORT)
+        })
+
+    } catch (e) {
+        console.log('server start error', e)
+    }
+}
+
+function checkConnection(req, res, next) {
+    if (isConnect) {
+        return next()
+    }
+
+    return errorResponse(res, {
+        status: 500,
+        errors: ['DB connect error']
+    })
+}
+
+await server()
