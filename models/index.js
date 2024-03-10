17be9dd4d049d1ff62922823bf1fdbfd357f3c1e
Trysten M <nick.tristen22@gmail.com>
2024-03-10 16:30:09 +0200

    add(api/models): add category model

--- models/index.js
+++ models/index.js
@@ -3,7 +3,9 @@ import {model} from "mongoose";
 import contactSchema from "../schemas/contactSchema.js";
 import skillSchema from "../schemas/skillSchema.js";
 import workSchema from "../schemas/workSchema.js";
+import categorySchema from "../schemas/categorySchema.js";
 
 export const Contact = model('Contact', contactSchema)
 export const Skill = model('Skill', skillSchema)
 export const Work = model('Work', workSchema)
+export const Category = model('Category', categorySchema)
