2a14d707bc4dffdc2eb166a06ce5abaf2070d8fe
Trysten M <nick.tristen22@gmail.com>
2024-03-23 18:48:39 +0200

    update(api/routes): add auth router to routes

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,12 @@ import _contactsRouter from "./_contactsRouter.js";
 import _skillsRouter from "./_skillsRouter.js";
 import _worksRouter from "./_worksRouter.js";
 import _categoriesRouter from "./_categoriesRouter.js";
+import _authRouter from "./_authRouter.js";
 
 const router = new Router()
-
-.use(_contactsRouter)
-.use(_skillsRouter)
-.use(_worksRouter)
-.use(_categoriesRouter)
+    .use(_authRouter)
+    .use(_contactsRouter)
+    .use(_skillsRouter)
+    .use(_worksRouter)
+    .use(_categoriesRouter)
 export default router
