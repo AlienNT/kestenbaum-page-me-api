b737e8a7ad030d7bd2be07e658a3811138f417d6
Trysten M <nick.tristen22@gmail.com>
2024-03-24 20:36:00 +0200

    update(api/middlewares): add check token

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,19 +1,30 @@
 import {errorResponse} from "../helpers/responseHelper.js";
 import statusCode from "../helpers/statusCodeHelper.js";
+import TokenService from "../services/tokenService.js";
 
-export function AuthMiddleware(req, res, next) {
+export async function AuthMiddleware(req, res, next) {
     if (req.method === "OPTIONS") {
         return next()
     }
 
     try {
+        const token = await TokenService.getToken(req)
+
+        if (!token) {
+            return errorResponse(res, {
+                status: statusCode.UNAUTHORIZED,
+                errors: ['не передан токен']
+            })
+        }
+
+
         return next() //todo добавить проверку по необходимости
     } catch (e) {
         console.log('AuthMiddleware error', e)
 
         return errorResponse(res, {
             status: statusCode.UNAUTHORIZED,
-            errors: ["user unauthorized *"]
+            errors: ["пользователь не авторизован *"]
         })
     }
 }
