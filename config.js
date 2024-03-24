e40770f5310a3bb4676dce499e58635eb6e038db
Trysten M <nick.tristen22@gmail.com>
2024-03-24 20:37:07 +0200

    update(api/config): add secret, refresh & cookie secure values to config

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
+import dayjs from 'dayjs'
 export default {
     DB: process.env.DB_URL,
     PORT: process.env.PORT || 5000,
-    API_ROUTE: process.env.API_ROUTE || '/api'
+    API_ROUTE: process.env.API_ROUTE || '/api',
+    SECRET: process.env.SECRET_KEY,
+    REFRESH_TOKEN_EXPIRES: dayjs().add(30, 'd').toDate(),
+    IS_COOKIE_SECURE: process.env.NODE_ENV !== "development"
 }
