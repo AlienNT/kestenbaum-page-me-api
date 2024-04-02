8de6a5ae4d315831b6aa2dcd7edf88975e2dc091
Trysten M <nick.tristen22@gmail.com>
2024-04-02 16:45:33 +0300

    update(api/routes): update logout route

--- routes/_authRouter.js
+++ routes/_authRouter.js
@@ -1,9 +1,20 @@
 import {Router} from "express";
 import {routeNames} from "../helpers/routesHelper.js";
 import AuthController from "../controllers/authController.js";
+import {AuthMiddleware} from "../middlewares/authMiddleware.js";
 
 const router = new Router()
-    .post(routeNames.AUTH.LOGIN, AuthController.login)
-    .post(routeNames.AUTH.LOGOUT, AuthController.logout)
-    .post(routeNames.AUTH.REGISTRATION, AuthController.registration)
+    .post(
+        routeNames.AUTH.LOGIN,
+        AuthController.login
+    )
+    .get(
+        routeNames.AUTH.LOGOUT,
+        AuthMiddleware,
+        AuthController.logout
+    )
+    .post(
+        routeNames.AUTH.REGISTRATION,
+        AuthController.registration
+    )
 export default router
