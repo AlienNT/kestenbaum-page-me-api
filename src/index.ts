9690e9a6b3c4998cdc44d4060ff5a738a2c049d1
Trysten M <nick.tristen22@gmail.com>
2024-04-23 17:06:44 +0300

    update(app): add show route to console middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express, {Express, Response, Request} from 'express'
+import express, {Express, Response, Request, NextFunction} from 'express'
 import dotenv from 'dotenv'
 import url from "url";
 import cookieParser from "cookie-parser";
+import {CustomRequest} from "./aliennt/webfolio-api/types.js";
 
 import tokensMiddleware from "./aliennt/webfolio-api/middlewares/tokensMiddleware.js";
 
@@ -20,6 +21,7 @@ const filePath: string = __dirname + '../views'
 dotenv.config()
 
 const API: Express = express()
+    .use(routeLogger as any)
     .use(cookieParser())
     .use(bodyParser.json({limit: "10mb"}))
     .use(
@@ -49,10 +51,15 @@ async function server() {
         API.get('/', (req: Request, res: Response) => {
             res.sendFile(filePath + '/index.html')
         })
-
+        console.log('mode', process.env.NODE_ENV)
     } catch (e) {
         console.log('server start error', e)
     }
+}
+
+function routeLogger(req: CustomRequest, res: Response, next: NextFunction) {
+    console.log('route: ', req.path)
+    next()
 }
 
 server().then(() => console.log('server start success'))
